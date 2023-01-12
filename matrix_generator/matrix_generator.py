ebc9338f45899fc76197ac6de155b649fb64eaa1
mauribadnights <mbuendiasilva@gmail.com>
2023-01-12 15:51:46 +0100

    Finished matrix generator

--- matrix_generator/matrix_generator.py
+++ matrix_generator/matrix_generator.py
@@ -6,6 +6,9 @@ from libraries.gridManipulation import *
 from data_preparator import *
 import numpy as np
 from collections import Counter
+import time
+import csv
+start_time = time.time()
 print('Libraries have been properly imported.')
 
 print('Declaring grid configuration:')
@@ -49,7 +52,8 @@ grid = create_grid(o, patch_size, total_patches, grid_angle)
 print('grid succesfully created')
 
 print('Downloading day data...')
-data = getDay(20220314,'ehv')
+day_to_analyze = input('What day do you want to analyze?:')
+data = getDay(day_to_analyze,'ehv')
 print('day data succesfully downloaded.')
 print('Removing data from outside bounds...')
 data = data.drop(data[data.x_pos < o_x].index)
@@ -82,20 +86,378 @@ def zone(x_pos, y_pos):
     return int(zone)
 print('zone indentifier ready.')
 
-print('Processing data_off...')
-data_off['zone'] = np.vectorize(zone)(data_off['x_pos'], data_off['y_pos'])
-print('...data_off processed.')
+print('Initializing markov matrix to appropiate size')
+markov_matrix = []
+for i in range(abs_total_patches+1):
+    markov_matrix.append([])
+    for j in range(abs_total_patches+1):
+        markov_matrix[i].append(0)
+print('Markov matrix initialized succesfully')
 
-print('Processing data_on_no_train...')
-data_on_no_train['zone'] = np.vectorize(zone)(data_on_no_train['x_pos'], data_on_no_train['y_pos'])
-print('...data_on_no_train processed.')
+print('Initializing object routes dictionary')
+object_routes = {}
+for x in (data_off.tracked_object.unique()):
+    object_routes[x] = []
+print('Initialized succesfully')
 
-print('Processing data_on_train_top...')
-data_on_train_top['zone'] = np.vectorize(zone)(data_on_train_top['x_pos'], data_on_train_top['y_pos'])
-print('...data_on_train_top processed.')
+print('Calculating pedestrian routes')
+sample_period = 1
+all_object_routes = []
+for x in object_routes:
+    object_data = data_off[data_off['tracked_object'] == x].reset_index().drop('index', axis=1)
+    for y in range(0, len(object_data)-1, sample_period*10):
+        curr_patch = zone(object_data.x_pos[y],object_data.y_pos[y])
+        object_routes[x].append(curr_patch)
+    object_routes[x].append(-1)
+    all_object_routes.extend(object_routes[x])
+print('Routes calculated')
 
-print('Processing data_on_train_bottom...')
-data_on_train_bottom['zone'] = np.vectorize(zone)(data_on_train_bottom['x_pos'], data_on_train_bottom['y_pos'])
-print('...data_on_train_bottom processed.')
+print('Counting movements and transitions')
+change_count = {}
+zone_appearance_count = {}
+for zone in range(len(all_object_routes)):
+    curr_zone = all_object_routes[zone]
+    
+    if (zone+1 != len(all_object_routes)) and (curr_zone != -1):
+        next_zone = all_object_routes[zone+1]
+        
+        if ((curr_zone,next_zone) in change_count):
+            change_count[(curr_zone,next_zone)] += 1
+        else:
+            change_count[(curr_zone,next_zone)] = 1
+        
+        if (curr_zone in zone_appearance_count):
+            zone_appearance_count[(curr_zone)] += 1
+        else:
+            zone_appearance_count[(curr_zone)] = 1
+print('Counting finished succesfully')
 
-print('Starting transition and zone count, this will take around 6 hours per day...')
+print('Calculating frequencies')
+change_probabilities = {}
+for zone_1, zone_2 in change_count:
+    change_probabilities[(zone_1,zone_2)] = change_count[(zone_1,zone_2)]/zone_appearance_count[zone_1]
+    markov_matrix[zone_1][zone_2] = change_probabilities[(zone_1,zone_2)]
+print('Frequencies succesfully calculated')
+
+print('Exporting result to matrix')
+result_name_and_path = 'results/offboarding/'+day_to_analyze+'.csv'
+with open(result_name_and_path, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(markov_matrix)
+f.close()
+
+
+
+
+
+
+
+
+
+
+def zone(x_pos, y_pos):
+    origin = o
+    size = patch_size
+    
+    i = np.floor((x_pos-origin[0])/size[0])
+    j = np.floor((y_pos-origin[1])/size[1])
+    
+    total_x_patches = total_patches[0]
+    total_y_patches = total_patches[1]
+    
+    zone = (i*total_y_patches)+j
+    return int(zone)
+
+print('Initializing markov matrix to appropiate size')
+markov_matrix_on_no = []
+for i in range(abs_total_patches+1):
+    markov_matrix_on_no.append([])
+    for j in range(abs_total_patches+1):
+        markov_matrix_on_no[i].append(0)
+print('Markov matrix initialized succesfully')
+
+print('Initializing object routes dictionary')
+object_routes_on_no = {}
+for x in (data_on_no_train.tracked_object.unique()):
+    object_routes_on_no[x] = []
+print('Initialized succesfully')
+
+print('Calculating pedestrian routes')
+all_object_routes_on_no = []
+for x in object_routes_on_no:
+    object_data = data_on_no_train[data_on_no_train['tracked_object'] == x].reset_index().drop('index', axis=1)
+    for y in range(0, len(object_data)-1, sample_period*10):
+        curr_patch = zone(object_data.x_pos[y],object_data.y_pos[y])
+        object_routes_on_no[x].append(curr_patch)
+    object_routes_on_no[x].append(-1)
+    all_object_routes_on_no.extend(object_routes_on_no[x])
+print('Routes calculated')
+
+print('Counting movements and transitions')
+change_count_on_no = {}
+zone_appearance_count_on_no = {}
+for zone in range(len(all_object_routes_on_no)):
+    curr_zone = all_object_routes_on_no[zone]
+    
+    if (zone+1 != len(all_object_routes_on_no)) and (curr_zone != -1):
+        next_zone = all_object_routes_on_no[zone+1]
+        
+        if ((curr_zone,next_zone) in change_count_on_no):
+            change_count_on_no[(curr_zone,next_zone)] += 1
+        else:
+            change_count_on_no[(curr_zone,next_zone)] = 1
+        
+        if (curr_zone in zone_appearance_count_on_no):
+            zone_appearance_count_on_no[(curr_zone)] += 1
+        else:
+            zone_appearance_count_on_no[(curr_zone)] = 1
+print('Counting finished succesfully')
+
+print('Calculating frequencies')
+change_probabilities_on_no = {}
+for zone_1, zone_2 in change_count_on_no:
+    change_probabilities_on_no[(zone_1,zone_2)] = change_count_on_no[(zone_1,zone_2)]/zone_appearance_count_on_no[zone_1]
+    markov_matrix_on_no[zone_1][zone_2] = change_probabilities_on_no[(zone_1,zone_2)]
+print('Frequencies succesfully calculated')
+
+print('Exporting result to matrix')
+result_name_and_path_on_no = 'results/onboarding_no_train/'+str(day_to_analyze)+'.csv'
+with open(result_name_and_path_on_no, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(markov_matrix_on_no)
+f.close()
+
+
+
+
+
+
+def zone(x_pos, y_pos):
+    origin = o
+    size = patch_size
+    
+    i = np.floor((x_pos-origin[0])/size[0])
+    j = np.floor((y_pos-origin[1])/size[1])
+    
+    total_x_patches = total_patches[0]
+    total_y_patches = total_patches[1]
+    
+    zone = (i*total_y_patches)+j
+    return int(zone)
+
+print('Initializing markov matrix to appropiate size')
+markov_matrix_on_top = []
+for i in range(abs_total_patches+1):
+    markov_matrix_on_top.append([])
+    for j in range(abs_total_patches+1):
+        markov_matrix_on_top[i].append(0)
+print('Markov matrix initialized succesfully')
+
+print('Initializing object routes dictionary')
+object_routes_on_top = {}
+for x in (data_on_train_top.tracked_object.unique()):
+    object_routes_on_top[x] = []
+print('Initialized succesfully')
+
+print('Calculating pedestrian routes')
+all_object_routes_on_top = []
+for x in object_routes_on_top:
+    object_data = data_on_train_top[data_on_train_top['tracked_object'] == x].reset_index().drop('index', axis=1)
+    for y in range(0, len(object_data)-1, sample_period*10):
+        curr_patch = zone(object_data.x_pos[y],object_data.y_pos[y])
+        object_routes_on_top[x].append(curr_patch)
+    object_routes_on_top[x].append(-1)
+    all_object_routes_on_top.extend(object_routes_on_top[x])
+print('Routes calculated')
+
+print('Counting movements and transitions')
+change_count_on_top = {}
+zone_appearance_count_on_top = {}
+for zone in range(len(all_object_routes_on_top)):
+    curr_zone = all_object_routes_on_top[zone]
+    
+    if (zone+1 != len(all_object_routes_on_top)) and (curr_zone != -1):
+        next_zone = all_object_routes_on_top[zone+1]
+        
+        if ((curr_zone,next_zone) in change_count_on_top):
+            change_count_on_top[(curr_zone,next_zone)] += 1
+        else:
+            change_count_on_top[(curr_zone,next_zone)] = 1
+        
+        if (curr_zone in zone_appearance_count_on_top):
+            zone_appearance_count_on_top[(curr_zone)] += 1
+        else:
+            zone_appearance_count_on_top[(curr_zone)] = 1
+print('Counting finished succesfully')
+
+print('Calculating frequencies')
+change_probabilities_on_top = {}
+for zone_1, zone_2 in change_count_on_top:
+    change_probabilities_on_top[(zone_1,zone_2)] = change_count_on_top[(zone_1,zone_2)]/zone_appearance_count_on_top[zone_1]
+    markov_matrix_on_top[zone_1][zone_2] = change_probabilities_on_top[(zone_1,zone_2)]
+print('Frequencies succesfully calculated')
+
+print('Exporting result to matrix')
+result_name_and_path_on_top = 'results/onboarding_top/'+str(day_to_analyze)+'.csv'
+with open(result_name_and_path_on_top, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(markov_matrix_on_top)
+f.close()
+
+
+
+
+
+def zone(x_pos, y_pos):
+    origin = o
+    size = patch_size
+    
+    i = np.floor((x_pos-origin[0])/size[0])
+    j = np.floor((y_pos-origin[1])/size[1])
+    
+    total_x_patches = total_patches[0]
+    total_y_patches = total_patches[1]
+    
+    zone = (i*total_y_patches)+j
+    return int(zone)
+
+print('Initializing markov matrix to appropiate size')
+markov_matrix_on_bottom = []
+for i in range(abs_total_patches+1):
+    markov_matrix_on_bottom.append([])
+    for j in range(abs_total_patches+1):
+        markov_matrix_on_bottom[i].append(0)
+print('Markov matrix initialized succesfully')
+
+print('Initializing object routes dictionary')
+object_routes_on_bottom = {}
+for x in (data_on_train_bottom.tracked_object.unique()):
+    object_routes_on_bottom[x] = []
+print('Initialized succesfully')
+
+print('Calculating pedestrian routes')
+all_object_routes_on_bottom = []
+for x in object_routes_on_bottom:
+    object_data = data_on_train_bottom[data_on_train_bottom['tracked_object'] == x].reset_index().drop('index', axis=1)
+    for y in range(0, len(object_data)-1, sample_period*10):
+        curr_patch = zone(object_data.x_pos[y],object_data.y_pos[y])
+        object_routes_on_bottom[x].append(curr_patch)
+    object_routes_on_bottom[x].append(-1)
+    all_object_routes_on_bottom.extend(object_routes_on_bottom[x])
+print('Routes calculated')
+
+print('Counting movements and transitions')
+change_count_on_bottom = {}
+zone_appearance_count_on_bottom = {}
+for zone in range(len(all_object_routes_on_bottom)):
+    curr_zone = all_object_routes_on_bottom[zone]
+    
+    if (zone+1 != len(all_object_routes_on_bottom)) and (curr_zone != -1):
+        next_zone = all_object_routes_on_bottom[zone+1]
+        
+        if ((curr_zone,next_zone) in change_count_on_bottom):
+            change_count_on_bottom[(curr_zone,next_zone)] += 1
+        else:
+            change_count_on_bottom[(curr_zone,next_zone)] = 1
+        
+        if (curr_zone in zone_appearance_count_on_bottom):
+            zone_appearance_count_on_bottom[(curr_zone)] += 1
+        else:
+            zone_appearance_count_on_bottom[(curr_zone)] = 1
+print('Counting finished succesfully')
+
+print('Calculating frequencies')
+change_probabilities_on_bottom = {}
+for zone_1, zone_2 in change_count_on_bottom:
+    change_probabilities_on_bottom[(zone_1,zone_2)] = change_count_on_bottom[(zone_1,zone_2)]/zone_appearance_count_on_bottom[zone_1]
+    markov_matrix_on_bottom[zone_1][zone_2] = change_probabilities_on_bottom[(zone_1,zone_2)]
+print('Frequencies succesfully calculated')
+
+print('Exporting result to matrix')
+result_name_and_path_on_bottom = 'results/onboarding_bottom/'+str(day_to_analyze)+'.csv'
+with open(result_name_and_path_on_bottom, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(markov_matrix_on_bottom)
+f.close()
+
+
+
+
+
+
+def zone(x_pos, y_pos):
+    origin = o
+    size = patch_size
+    
+    i = np.floor((x_pos-origin[0])/size[0])
+    j = np.floor((y_pos-origin[1])/size[1])
+    
+    total_x_patches = total_patches[0]
+    total_y_patches = total_patches[1]
+    
+    zone = (i*total_y_patches)+j
+    return int(zone)
+
+print('Initializing markov matrix to appropiate size')
+markov_matrix_on_both = []
+for i in range(abs_total_patches+1):
+    markov_matrix_on_both.append([])
+    for j in range(abs_total_patches+1):
+        markov_matrix_on_both[i].append(0)
+print('Markov matrix initialized succesfully')
+
+print('Initializing object routes dictionary')
+object_routes_on_both = {}
+for x in (data_on_train_both.tracked_object.unique()):
+    object_routes_on_both[x] = []
+print('Initialized succesfully')
+
+print('Calculating pedestrian routes')
+all_object_routes_on_both = []
+for x in object_routes_on_both:
+    object_data = data_on_train_both[data_on_train_both['tracked_object'] == x].reset_index().drop('index', axis=1)
+    for y in range(0, len(object_data)-1, sample_period*10):
+        curr_patch = zone(object_data.x_pos[y],object_data.y_pos[y])
+        object_routes_on_both[x].append(curr_patch)
+    object_routes_on_both[x].append(-1)
+    all_object_routes_on_both.extend(object_routes_on_both[x])
+print('Routes calculated')
+
+print('Counting movements and transitions')
+change_count_on_both = {}
+zone_appearance_count_on_both = {}
+for zone in range(len(all_object_routes_on_both)):
+    curr_zone = all_object_routes_on_both[zone]
+    
+    if (zone+1 != len(all_object_routes_on_both)) and (curr_zone != -1):
+        next_zone = all_object_routes_on_both[zone+1]
+        
+        if ((curr_zone,next_zone) in change_count_on_both):
+            change_count_on_both[(curr_zone,next_zone)] += 1
+        else:
+            change_count_on_both[(curr_zone,next_zone)] = 1
+        
+        if (curr_zone in zone_appearance_count_on_both):
+            zone_appearance_count_on_both[(curr_zone)] += 1
+        else:
+            zone_appearance_count_on_both[(curr_zone)] = 1
+print('Counting finished succesfully')
+
+print('Calculating frequencies')
+change_probabilities_on_both = {}
+for zone_1, zone_2 in change_count_on_both:
+    change_probabilities_on_both[(zone_1,zone_2)] = change_count_on_both[(zone_1,zone_2)]/zone_appearance_count_on_both[zone_1]
+    markov_matrix_on_both[zone_1][zone_2] = change_probabilities_on_both[(zone_1,zone_2)]
+print('Frequencies succesfully calculated')
+
+print('Exporting result to matrix')
+result_name_and_path_on_both = 'results/onboarding_both/'+str(day_to_analyze)+'.csv'
+with open(result_name_and_path_on_both, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(markov_matrix_on_both)
+f.close()
+
+
+end_time = time.time()
+
+print(end_time-start_time)
